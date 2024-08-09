999dc3dccf115f3540f3233af0577ae082172473
Surendar K <surendarkumar2k@gmail.com>
2024-08-09 17:15:14 +0530

    api commit

--- coffee-web/src/components/Login/firebase.js
+++ coffee-web/src/components/Login/firebase.js
@@ -1,19 +1,18 @@
-
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
-import {getFirestore} from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhXFOkiyV5TytzyuLbHOuJc3YehO9gZe8",
-  authDomain: "caffeeespot.firebaseapp.com",
-  projectId: "caffeeespot",
-  storageBucket: "caffeeespot.appspot.com",
-  messagingSenderId: "768055799569",
-  appId: "1:768055799569:web:667957365c64d3966a26b0",
-  measurementId: "G-W6F80Y2Y4S"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db=getFirestore(app);
-export const auth=getAuth();
+export const db = getFirestore(app);
+export const auth = getAuth();
